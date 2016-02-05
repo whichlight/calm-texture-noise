076f6d33338496ac22b6598ee06ed988f7179974
Kawandeep Virdee <whichlight@gmail.com>
2016-02-04 23:05:34 -0500

    updating noise

--- sketch.js
+++ sketch.js
@@ -1,12 +1,13 @@
 //touch interaction var
 var m = {x:0, y:0};
+var noises = [];
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   grow = windowHeight;
   background(0);
-
+  initSynth();
 
   //disable default touch events for mobile
   var el = document.getElementsByTagName("canvas")[0];
@@ -15,6 +16,11 @@ function setup() {
   el.addEventListener("touchcancel", pdefault, false);
   el.addEventListener("touchleave", pdefault, false);
   el.addEventListener("touchmove", pdefault, false);
+
+
+  stroke(255);
+  strokeWeight(10);
+  line(0,height/1.6,width, height/1.6);
 }
 
 function windowResized() {
@@ -36,16 +42,25 @@ function update(){
   m.y = max(touchY, mouseY);
   m.pressed = mouseIsPressed || touchIsDown;
 
-  if(m.y>0){
-    audiofilter.frequency.value = height-m.y;
+  if(m.pressed){
+
+   base_note = map(m.y, 0,height, 80,20);
+   console.log(base_note);
+    noises.forEach(function(n){
+      n.setBaseNote(base_note);
+
+    });
+
+  background(0);
+  stroke(255);
+  strokeWeight(10);
+  line(0,m.y,width, m.y);
   }
-//  audiofilter.Q.value= m.y;
 }
 
 function render(){
 
 }
-
 
 
 
@@ -71,46 +86,69 @@ function initSynth(){
   }
 
   if(typeof(context)!="undefined"){
-
+    startSynth();
   }
 }
 
 
-var whiteNoiseGen = function(){
-
-  var bufferSize = 4096; //Math.pow(2,13); //between 8 & 14
-
-  //make this a global var so it isnt garbage collected
-  whiteNoise = context.createScriptProcessor(bufferSize, 0, 2);
-  whiteNoise.onaudioprocess = function(e) {
-    var outputBuffer = e.outputBuffer;
-    for (var channel = 0; channel < outputBuffer.numberOfChannels; channel++) {
-      var outputData = outputBuffer.getChannelData(channel);
-      for (var i = 0; i < bufferSize; i++) {
-        outputData[i] = Math.random()*2-1;
+function Noise(args){
+  this.note= args.base_note + args.offset;
+  this.offset = args.offset;
+  this.whiteNoise;
+  this.filter;
+  this.volume = args.volume;
+  this.init = function(){
+    var bufferSize = 4096; //Math.pow(2,13); //between 8 & 14
+    //make this a global var so it isnt garbage collected
+    this.whiteNoise = context.createScriptProcessor(bufferSize, 0, 2);
+    this.whiteNoise.onaudioprocess = function(e) {
+      var outputBuffer = e.outputBuffer;
+      for (var channel = 0; channel < outputBuffer.numberOfChannels; channel++) {
+        var outputData = outputBuffer.getChannelData(channel);
+        for (var i = 0; i < bufferSize; i++) {
+          outputData[i] = Math.random()*2-1;
+        }
       }
     }
+
+
+    this.filter= context.createBiquadFilter();
+    this.filter.type = "lowpass";
+    this.filter.frequency.value = mtof(this.note);
+    this.filter.Q.value= 1;
+
+    this.gain = context.createGain();
+    this.gain.connect(context.destination);
+    this.gain.gain.value = this.volume;
+
+    this.whiteNoise.connect(this.filter);
+    this.filter.connect(this.gain);
+
+    var convolver = context.createConvolver();
+    console.log('create');
+
   }
 
+ this.setBaseNote = function(bn){
+    this.note = bn + this.offset;
+    this.filter.frequency.value = mtof(this.note);
+  }
 
-  var gain = context.createGain();
-  gain.gain.value = 0.8;
-
-  audiofilter = context.createBiquadFilter();
-  var convolver = context.createConvolver();
-
-  audiofilter.type = "bandpass";
-  audiofilter.frequency.value = 600;
-  audiofilter.Q.value= 1;
-  audiofilter.gain.value = 100;
-
-  whiteNoise.connect(audiofilter);
-  audiofilter.connect(gain);
-  gain.connect(context.destination);
+  this.init();
 
 
 }
 
+function mtof(m) {
+  return Math.pow(2, (m - 69) / 12) * 440;
+}
 
-initSynth();
-whiteNoiseGen();
+var startSynth = function(){
+  base_note = 50;
+
+  noises.push(new Noise({base_note:base_note/2, offset:0, volume: 0.9}));
+  noises.push(new Noise({base_note:base_note, offset:0, volume: 0.9}));
+  noises.push(new Noise({base_note:base_note,  offset:16, volume: 0.5}));
+  noises.push(new Noise({base_note:base_note,  offset:12, volume: 0.5}));
+}
+
